28a80007b4a7cbb9dfe6a96d6f024d90845358b2
Sergey Kropachev <kropachev_1998@mail.ru>
2024-12-12 12:37:17 +0500

    make migrations(product & category)

--- uppiesshop/shop/models.py
+++ uppiesshop/shop/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.forms import CharField
 
 
-class Categoty(models.Models):
+class Categoty(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
 
