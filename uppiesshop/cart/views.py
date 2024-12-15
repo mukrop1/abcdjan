e8eba03ea2525a72b27d8ad1c6d010a4793403ef
Sergey Kropachev <kropachev_1998@mail.ru>
2024-12-15 15:24:46 +0500

    all otheers for cart

--- uppiesshop/cart/views.py
+++ uppiesshop/cart/views.py
@@ -9,7 +9,7 @@ from .forms import CartAddProductForm
 def cart_add(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
-    form = CartAddProductForm(request.Post)
+    form = CartAddProductForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
         cart.add(
@@ -17,13 +17,22 @@ def cart_add(request, product_id):
         )
     return redirect("cart:cart_detail")
 
+
 @require_POST
 def cart_remove(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     cart.remove(product)
-    return redirect('cart:cart_detail')
+    return redirect("cart:cart_detail")
+
 
 def cart_detail(request):
     cart = Cart(request)
-    return render (request, 'cart/detail.html', {'cart': cart})
+    for item in cart:
+        item["update_quantity_form"] = CartAddProductForm(
+            initial={
+                "quantity": item["quantity"],
+                "override": True,
+            }
+        )
+    return render(request, "cart/detail.html", {"cart": cart})
