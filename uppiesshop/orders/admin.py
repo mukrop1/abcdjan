6277a8bacd868d90a09138bfebb1bc2cc6166562
Sergey Kropachev <kropachev_1998@mail.ru>
2024-12-17 18:41:16 +0500

    added link in admin panel in Orders to each order

--- uppiesshop/orders/admin.py
+++ uppiesshop/orders/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Order, OrderItem
 
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ["product"]
+
+
+def order_stripe_payment(obj):
+    url = obj.get_stripe_url()
+    if obj.stripe_id:
+        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+        return mark_safe(html)
+    return ""
+
+order_stripe_payment.short_description = "Stripe payment"
 
 
 @admin.register(Order)
@@ -18,6 +29,7 @@ class OrderAdmin(admin.ModelAdmin):
         "postal_code",
         "city",
         "paid",
+        order_stripe_payment,
         "created",
         "updated",
     ]
